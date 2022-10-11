53dcf6aa0843bab2ffaf5f7fb06ab5a799b5f4e3
mayankS07 <97273666+mayankS07@users.noreply.github.com>
2022-10-11 19:40:21 +0530

    Update README.R

--- README.R
+++ README.R
@@ -1,4 +1,5 @@
 # Lexical-Scoping
+
 makeCacheMatrix <- function(x = matrix()) {
   # todo error if x is not a matrix
   inv <- NULL
@@ -15,14 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-#' Compute and cache the inverse of a matrix
-#' @param x the result of a previous makeCacheMatrix call
-#' @param ... additional arguments to pass to solve function
-#' examples
-#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-#' cacheSolve(x)
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached matrix inverse")
